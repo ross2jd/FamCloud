c951aba062196ff84287c07f00dba03ca762d42f
Jordan Ross <ross.j101@gmail.com>
2014-05-21 09:24:32 -0400

    Created the outline for the music player page in terms of divs so I can plug in the components.

---
+++ musicPlayer.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <style>
+        .topBar {
+            width: 100%;
+            height: 114px;
+            margin: 0 0 0 0;
+        }
+        .jPlayerUI {
+            width: 420px;
+            height: 114px;
+            background-color: #FFFF00;
+            float: left;
+        }
+        .topBarPadLeft {
+            background-color: #FF0000;
+            float: left;
+            
+        }
+        .topBarPadRight {
+            background-color: #FF0000;
+            float: left;
+        }
+    </style>
+</head>
+
+<body>
+    <!-- The following code is for the top bar of music player -->
+    <div class="topBar">
+        <div class="topBarPadLeft">
+            
+        </div>
+        <div class="jPlayerUI">
+            <!-- The jPlayer skin that we are currently using is 420px wide and 114px tall -->
+        </div>
+        <div class="topBarPadRight">
+            
+        </div>
+    </div>
+    
+</body>
+<?php
+
+?>
